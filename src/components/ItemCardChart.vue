51cee7e49942d2f5ca35bdeede73ecbd42a03365
weiwu <645046307@qq.com>
2017-05-08 23:33:30 +0800

    完善样式

--- src/components/ItemCardChart.vue
+++ src/components/ItemCardChart.vue
@@ -54,28 +54,29 @@ export default {
     padding-top: 20px
     table
       width: 100%
-    tr
-      text-align: center
-      @media (max-width: 768px)
-        font-size: 13px
-      // th
-        // width: 20%
-      th
-        width: 14.6%
-        &:first-child
-          width: 100px
-          width: 27%
-      td
-        padding: 2px 0
-        // border-radius: 3px
-        // &:first-child
-        font-weight: 500
-        background: #333
-        color: #fff
-        &.free
-          // background: linear-gradient(to bottom, #5793f2, #4688f1)
-          background: #4688f1
-        &.busy
-          // background: linear-gradient(to bottom, #d86b72, #d85a63)
-          background: #d85a63
+      tr
+        text-align: center
+        @media (max-width: 768px)
+          font-size: 13px
+        th
+          width: 14.6%
+          &:first-child
+            // width: 100px
+            width: 27%
+        td
+          padding: 2px 0
+          // border-radius: 3px
+          // &:first-child
+          font-weight: 500
+          background: #333
+          color: #fff
+          min-height: 26px
+          &:first-child
+            line-height: 26px
+          &.free
+            // background: linear-gradient(to bottom, #5793f2, #4688f1)
+            background: #4688f1
+          &.busy
+            // background: linear-gradient(to bottom, #d86b72, #d85a63)
+            background: #d85a63
 </style>

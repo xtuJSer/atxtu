062d50dd68e678f2015fd81ff2deb89f36c6f17a
weiwu <645046307@qq.com>
2017-05-11 22:41:13 +0800

    增删注释

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   classroomURL: 'https://xtuapi.magicallu.cn/v1/user/classroom',
   messageURL: 'https://xtuapi.magicallu.cn/v1/user/classroom/message'
-  // classroomURL: 'http://localhost:3000/v1/user/classroom'
+  // classroomURL: 'http://localhost:3000/v1/user/classroom',
+  // messageURL: 'http://localhost:3000/v1/user/classroom/message'
 }

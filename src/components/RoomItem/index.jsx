bdcd439e5924e7cf8f1b2251e95705a243f727d9
weiwu <645046307@qq.com>
2017-09-02 17:14:11 +0800

    为空闲教室添加基本样式

--- src/components/RoomItem/index.jsx
+++ src/components/RoomItem/index.jsx
@@ -1,19 +1,49 @@
 import React, { Component } from 'react'
 
+import './style.styl'
+
 class RoomItem extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      sift: [false, false, false, false, false],
+      time: Array.apply(null, new Array(5))
+    }
+  }
+
   render () {
-    const build = this.props.data
+    const { name, rooms } = this.props.data
+    const { sift, time } = this.state
 
     return (
-      <li className="room-item">
-        <h3>{ build.name }</h3>
-        <ul>{
-          build.rooms.map((item, i) => (
-            <li key={ i }>
-              <h4>{ item.room }</h4>
-            </li>
-          ))
-        }</ul>
+      <li className="room-item card">
+        <h3>{ name }</h3>
+
+        <table>
+          <thead>
+            <tr>
+              <th id="title">
+                { sift.indexOf(true) > -1
+                  ? (sift.indexOf(false) > -1 ? '全选' : '取消')
+                  : '筛选' }
+              </th>
+              { time.map((item, i) => <th key={ i }>{ i }</th>) }
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              rooms.map((item, i) => (
+                <tr key={ i }>
+                  <td key="0">{ item.room }</td>
+                  { item.time.map((el, j) => (
+                    <td key={ j + 1 } className={ el === '满' ? 'busy' : 'free' }>{ el }</td>
+                  )) }
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
       </li>
     )
   }
--- src/containers/Room/sub/RoomList.jsx
+++ src/containers/Room/sub/RoomList.jsx
@@ -6,7 +6,7 @@ import '../style.styl'
 class RoomList extends Component {
   render () {
     return (
-      <ul className="room-list">
+      <ul className="room-list container">
         { this.props.data.map((item, i) => (
           <RoomItem key={ i } data={ item } />
         )) }
